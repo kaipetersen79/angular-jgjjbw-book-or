ed92d9cd551ad8652368ad186e39d3aa0c95132f
kaipetersen79 <kaipetersen79@gmail.com>
2019-10-11 10:15:07 +0200

    Completed Chapter 2 Hello Angular

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -15,10 +15,10 @@ export class ProductComponent implements OnInit {
    }
 
   ngOnInit() {
-    this.products.push(new Product("Ferrari", 15, "../model/bla.jpg"));
+    this.products.push(new Product("Ferrari", 15, "https://drop.ndtv.com/albums/AUTO/Maruti_Suzuki_S_637057817188112458/637057817210146726.jpeg"));
     this.products[0].onSale=true;
-    this.products.push(new Product("Porsche", 12, "../model/bla.jpg"));
-    this.products.push(new Product("VW", 14, "../model/bla.jpg"));
+    this.products.push(new Product("Porsche", 12, "https://drop.ndtv.com/albums/AUTO/porsche-taycan-turbo/1200x900_1.jpg"));
+    this.products.push(new Product("VW", 14, "https://drop.ndtv.com/albums/AUTO/Hyundai_Grand_i_637025058359468841/637025058404396378.jpeg"));
   }
 
 }
